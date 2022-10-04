167a84f8be73afd4bc3cf3577f719aa65baee4c4
kankit08 <kankit08@gmail.com>
2022-10-04 23:41:10 +0530

    bigPromise to deal with errors

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,9 +1,12 @@
-exports.home = (req, res) => {
+// importing Big Promise
+const bigPromise = require("../middlewares/bigPromise");
+
+exports.home = bigPromise((req, res) => {
   res.status(200).json({
     success: true,
     greetings: "Hello from T-shirt Store",
   });
-};
+});
 
 exports.dummyPage = (req, res) => {
   res.status(200).json({
