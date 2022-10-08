7117ef07c9626bb1a886634aebdd9b8422514602
kankit08 <kankit08@gmail.com>
2022-10-08 12:58:06 +0530

    logout Route

--- controllers/userController.js
+++ controllers/userController.js
@@ -66,3 +66,15 @@ exports.login = bigPromise(async (req, res, next) => {
   // If all good, send the token
   cookieToken(user, res);
 });
+
+// Logout Route
+exports.logout = bigPromise(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    message: "Logout Successfully",
+  });
+});
--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 //importig controller home
-const { signup, login } = require("../controllers/userController");
+const { signup, login, logout } = require("../controllers/userController");
 
 //routes
 router.route("/signup").post(signup);
 router.route("/login").post(login);
+router.route("/logout").get(logout);
 
 // exporting route
 module.exports = router;
