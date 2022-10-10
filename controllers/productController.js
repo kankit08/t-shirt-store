6b0a5487c23b7fd925afa43386bec8b2ef955b8b
kankit08 <kankit08@gmail.com>
2022-10-10 21:23:09 +0530

    adding a product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,9 +1,37 @@
+const Product = require("../models/product");
 const bigPromise = require("../middlewares/bigPromise");
 const customError = require("../utils/customError");
+const cloudinary = require("cloudinary");
 
-exports.productTest = bigPromise(async (req, res, next) => {
+exports.addProduct = bigPromise(async (req, res, next) => {
+  // images
+
+  let imageArray = [];
+  if (!req.files) {
+    return next(new customError("Images are required", 401));
+  }
+
+  if (req.files) {
+    for (let index = 0; index < req.files.photos.length; index++) {
+      let result = await cloudinary.v2.uploader.upload(
+        req.files.photos[index].tempFilePath,
+        {
+          folder: "products",
+        }
+      );
+      imageArray.push({
+        id: result.public_id,
+        secure_url: result.secure_url,
+      });
+    }
+  }
+
+  req.body.photos = imageArray;
+  req.body.user = req.user.id;
+
+  const product = await Product.create(req.body);
   res.status(200).json({
     success: true,
-    greetings: "This is a test",
+    product,
   });
 });
--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { productTest } = require("../controllers/productController");
+// const { productTest } = require("../controllers/productController");
 const { isLoggedIn, customRole } = require("../middlewares/user");
 
 // Routes
-router.route("/productTest").get(productTest);
+// router.route("/productTest").get(productTest);
 
 module.exports = router;
