a8ddc61208b3499292b3a2b06a20701dffbba13e
kankit08 <kankit08@gmail.com>
2022-10-10 21:36:01 +0530

    utils updated with a new func

---
+++ utils/whereClause.js
@@ -0,0 +1,17 @@
+class WhereClause {
+  constructor(base, bigQ) {
+    (this.base = base), (this.bigQ = bigQ);
+  }
+  search() {
+    const searchword = this.bigQ.search
+      ? {
+          name: {
+            $regex: this.bigQ.search,
+            $options: "i",
+          },
+        }
+      : {};
+    this.base = this.base.find({ ...searchword });
+    return this;
+  }
+}
