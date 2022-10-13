7ced2a1c8902702b629064f0cc6b29b0186898b4
kankit08 <kankit08@gmail.com>
2022-10-13 22:21:42 +0530

    razorpay payment method

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,5 +1,5 @@
 const bigPromise = require("../middlewares/bigPromise");
-const customError = require("../utils/customError");
+const { v4: uuidv4 } = require("uuid");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.sendStripeKey = bigPromise(async (req, res, next) => {
@@ -19,6 +19,33 @@ exports.captureStripePayment = bigPromise(async (req, res, next) => {
 
   res.status(200).json({
     success: true,
+    amount: req.body.amount,
     client_secret: paymentIntent.client_secret,
+  });
+});
+
+exports.sendRazorpayKey = bigPromise(async (req, res, next) => {
+  res.status(200).json({
+    key_id: process.env.RAZORPAY_API_KEY,
+  });
+});
+
+exports.captureRazorpayPayment = bigPromise(async (req, res, next) => {
+  var instance = new Razorpay({
+    key_id: process.env.RAZORPAY_API_KEY,
+    key_secret: process.env.RAZORPAY_SECRET_KEY,
+  });
+  var options = {
+    amount: req.body.amount,
+    currency: "INR",
+    receipt: uuidv4(),
+  };
+
+  const myOrder = await instance.orders.create(options);
+
+  res.status(200).json({
+    success: true,
+    amount: req.body.amount,
+    order: myOrder,
   });
 });
