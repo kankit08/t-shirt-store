c7d2733f25585a1d02caf5d549a1a536a9108553
kankit08 <kankit08@gmail.com>
2022-10-14 22:46:42 +0530

    oderController for createOrder

---
+++ controllers/orderController.js
@@ -0,0 +1,30 @@
+const Order = require("../models/order");
+const Product = require("../models/product");
+const bigPromise = require("../middlewares/bigPromise");
+const customError = require("../utils/customError");
+
+exports.createOrder = bigPromise(async (req, res, next) => {
+  const {
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    taxAmount,
+    shippingAmount,
+    totalAmount,
+  } = req.body;
+
+  const order = await Order.create({
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    taxAmount,
+    shippingAmount,
+    totalAmount,
+    user: req.user._id,
+  });
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
